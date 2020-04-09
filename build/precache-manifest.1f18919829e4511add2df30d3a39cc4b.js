422978fbca262af641b7dcba6d90c5e388bcc9b3
Alican <alican.torun@cirplus.io>
2020-04-09 15:55:42 +0200

    rm production from gitignore

---
+++ build/precache-manifest.1f18919829e4511add2df30d3a39cc4b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4177c1331ebecee1b2edf503e790e209",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c8a0ef2bd1114159d74",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "c0f36269c05a1ed44cf0",
+    "url": "/static/js/3.0c78a080.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/3.0c78a080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "174b245e37f70d5565d1",
+    "url": "/static/js/4.723f0dea.chunk.js"
+  },
+  {
+    "revision": "7b1c186c57c4967ab389",
+    "url": "/static/js/5.326820b2.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/static/js/5.326820b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f748e7f514fec75852b",
+    "url": "/static/js/6.793a84d5.chunk.js"
+  },
+  {
+    "revision": "c2703eee005e82dd04f5f76ec1204181",
+    "url": "/static/js/6.793a84d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c8a0ef2bd1114159d74",
+    "url": "/static/js/main.1c91ce57.chunk.js"
+  },
+  {
+    "revision": "38f21605449ba6f839eb",
+    "url": "/static/js/runtime-main.5482d12d.js"
+  },
+  {
+    "revision": "e51b0a004d0b4f8873bb",
+    "url": "/static/js/xlsx.1f1c32e3.chunk.js"
+  }
+]);
---
+++ build/static/js/xlsx.1f1c32e3.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpcorona-watch"]=this["webpackJsonpcorona-watch"]||[]).push([[2],{544:function(n,c){},632:function(n,c){},633:function(n,c){}}]);
+//# sourceMappingURL=xlsx.1f1c32e3.chunk.js.map
