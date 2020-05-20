8bc3b75f09ebc99132d588681908593d5cca06d7
Alican Torun <alican@Alicans-MBP.fritz.box>
2020-05-20 23:56:49 +0200

    env

---
+++ build/precache-manifest.c5bf7022916c9ccf8b857e24222c3fc1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c3a4fd73a9f9b3dc08bd7a3da5c3c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27cf18cf3f763d259454",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "a85d3c52cb38df4637f8",
+    "url": "/static/js/3.a1801c33.chunk.js"
+  },
+  {
+    "revision": "ec57064979281ac1559f738c10271bc7",
+    "url": "/static/js/3.a1801c33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "388c70d85199b646e37a",
+    "url": "/static/js/4.435a2d3a.chunk.js"
+  },
+  {
+    "revision": "f1bb0a106c8799cd250e",
+    "url": "/static/js/5.13e94f2f.chunk.js"
+  },
+  {
+    "revision": "c2703eee005e82dd04f5f76ec1204181",
+    "url": "/static/js/5.13e94f2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72f42745d83d0de1eaa6",
+    "url": "/static/js/6.2359a065.chunk.js"
+  },
+  {
+    "revision": "27cf18cf3f763d259454",
+    "url": "/static/js/main.02c0e531.chunk.js"
+  },
+  {
+    "revision": "3f61b83496b6c4fbf5a4",
+    "url": "/static/js/runtime-main.85ed03e8.js"
+  },
+  {
+    "revision": "223a4d094fbc781127b1",
+    "url": "/static/js/xlsx.9a1eb070.chunk.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6d93d48fa1be24ee725c09cc5fa0eedf.js"
+  "/precache-manifest.c5bf7022916c9ccf8b857e24222c3fc1.js"
 );
 
 self.addEventListener('message', (event) => {
